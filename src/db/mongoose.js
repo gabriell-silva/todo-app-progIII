404ca1e6a965077f3a4dcf4e456ada0cc3cf168a
Samuel Rodrigues <samuelclerod@gmail.com>
2022-04-19 19:06:20 -0300

    mongoose first class

---
+++ src/db/mongoose.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/task-manager-api', {
+  useNewUrlParser: true,
+})
+
+const User = mongoose.model('User', {
+  name: {
+    type: String,
+  },
+  age: {
+    type: Number,
+  }
+});
+
+const me = new User({ name: "Samuel Rodrigues", age: 36 });
+
+me.save()
+  .then((result) => console.log(result))
+  .catch((error) => console.log(error));
