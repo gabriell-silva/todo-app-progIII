5276de8958a1237b5ff7a79ece9e9b3d902765ec
Samuel Rodrigues <samuelclerod@gmail.com>
2022-04-19 19:09:25 -0300

    exec: create task model

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -4,17 +4,20 @@ mongoose.connect('mongodb://localhost:27017/task-manager-api', {
   useNewUrlParser: true,
 })
 
-const User = mongoose.model('User', {
-  name: {
-    type: String,
-  },
-  age: {
-    type: Number,
-  }
-});
+//TODO description: string  e completed: boolean
 
-const me = new User({ name: "Samuel Rodrigues", age: 36 });
 
-me.save()
-  .then((result) => console.log(result))
-  .catch((error) => console.log(error));
+// const User = mongoose.model('User', {
+//   name: {
+//     type: String,
+//   },
+//   age: {
+//     type: Number,
+//   }
+// });
+
+// const me = new User({ name: "Samuel Rodrigues", age: 36 });
+
+// me.save()
+//   .then((result) => console.log(result))
+//   .catch((error) => console.log(error));
